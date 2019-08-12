6e00995767a6e063b3dbb1e2cac61109b104f37f
thrupence222 <thrupence08@gmail.com>
2019-08-12 13:59:27 +0300

    update2

---
+++ landing1/js/main.js
@@ -0,0 +1,54 @@
+	
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+{
+	nav:true,
+ loop:true,
+}
+
+  	);
+});
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+Date.prototype.format = function(format = 'yyyy-mm-dd') {
+    const replaces = {
+        yyyy: this.getFullYear(),
+        mm: ('0'+(this.getMonth() + 1)).slice(-2),
+        dd: ('0'+this.getDate()).slice(-2),
+        hh: ('0'+this.getHours()).slice(-2),
+        MM: ('0'+this.getMinutes()).slice(-2),
+        ss: ('0'+this.getSeconds()).slice(-2)
+    };
+    let result = format;
+    for(const replace in replaces){
+        result = result.replace(replace,replaces[replace]);
+    }
+    return result;
+};
+
+setInterval(function(){
+    document.getElementById("time").innerHTML = new Date().format('hh:MM:ss dd.mm.yyyy');
+},1000);
+
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
+
+function thanks(){
+alert("Спасибо!");
+}
